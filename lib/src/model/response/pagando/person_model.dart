86e7180def803574c8d1344a4c0668934e3f9b6f
Freddy Campos <marthianfred@gmail.com>
2024-07-01 11:56:13 -0400

    add model user to Person Model

--- lib/src/model/response/pagando/person_model.dart
+++ lib/src/model/response/pagando/person_model.dart
@@ -1,34 +1,28 @@
 import 'package:paying_service/service.dart';
 
-/// Model for the response of the person
 class PersonModel {
-  /// Constructor with required parameters
   PersonModel({required this.message, required this.data});
 
-  /// Factory constructor with required parameters
   factory PersonModel.fromJson(Map<String, dynamic> json) {
     return PersonModel(
-      message: json['message'] != null ? json['message'].toString() : '',
+      message: json['message'],
       data: DataPersonModel.fromJson(json['data'] as Map<String, dynamic>),
     );
   }
 
-  /// [message] is the message of the response
   final String message;
 
-  /// [data] is the data of the person
   final DataPersonModel data;
 }
 
-/// Model for the response of the person
 class DataPersonModel {
-  /// Constructor with required parameters
   DataPersonModel({
     required this.id,
     required this.firstName,
     required this.secondName,
     required this.dni,
     required this.dniType,
+    this.user,
   });
 
   factory DataPersonModel.fromJson(Map<String, dynamic> json) {
@@ -38,21 +32,19 @@ class DataPersonModel {
       secondName: json['secondName'].toString(),
       dni: json['dni'].toString(),
       dniType: DniType.fromJson(json['dniType'] as Map<String, dynamic>),
+      user: json['user'] ?? null,
     );
   }
 
-  /// [id] is the id of the person
   final String id;
 
-  /// [firstName] is the first name of the person
   final String firstName;
 
-  /// [secondName] is the second name of the person
   final String secondName;
 
-  /// [dni] is the dni of the person
   final String dni;
 
-  /// [dniType] is the type of the dni of the person
   final DniType dniType;
+
+  final User? user;
 }
